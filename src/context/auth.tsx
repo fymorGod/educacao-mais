ab475788cb89bbe81e98b92741409b21a20ef853
fymorGod <mufinagamer@gmail.com>
2022-10-31 14:56:57 -0300

    add

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -3,23 +3,14 @@ import { useNavigate } from "react-router-dom";
 import { app, createSession } from "../api/app";
 import { AuthContextType } from "../types/AuthContextType";
 import { User } from "../types/User";
-import {
-  getDatabase,
-  get,
-  ref,
-  set,
-  onValue,
-  push,
-  update,
-} from 'firebase/database';
+
 
 export const AuthContext = createContext<AuthContextType>(null!);
 
 export const AuthProvider = ({children}: {children: React.ReactNode}) => {
-  const [myData, setMyData] = useState(null);
   const navigate = useNavigate();
   const [user, setUser] = useState<string>();
-  const [users, setUsers] = useState<string[]>([]);
+
 
   useEffect(() => {
     const storageData  = localStorage.getItem("user");
@@ -29,48 +20,7 @@ export const AuthProvider = ({children}: {children: React.ReactNode}) => {
     }
   }, []);
 
-  const findUser = async (name: any) => {
-    const database = getDatabase();
 
-    const mySnapshot = await get(ref(database, users/${name}));
-
-    return mySnapshot.val();
-  };
-
-  const onLogin = async (username) => {
-    try {
-      const database = getDatabase();
-      //first check if the user registered before
-
-      const user = await findUser(username);
-
-      //create a new user if not registered
-      if (user) {
-        setMyData(user);
-      } else {
-        const newUserObj = {
-          username: username,
-          avatar: 'https://i.pravatar.cc/150?u=' + Date.now(),
-        };
-
-        set(ref(database, users/${username}), newUserObj);
-        setMyData(newUserObj);
-      }
-
-      // set friends list change listener
-      const myUserRef = ref(database, users/${username});
-      onValue(myUserRef, snapshot => {
-        const data = snapshot.val();
-        setUsers(data.friends);
-        setMyData(prevData => ({
-          ...prevData,
-          friends: data.friends,
-        }));
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   const login = async (mat: string, password: string) => {
     const response = await createSession(mat, password);
